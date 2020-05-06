58e2f2734b4dd9ffc791da65b4f33de3df457915
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2020-05-06 15:54:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const http = require('http');
+
+const hostname = process.env.hostname || '0.0.0.0';
+const port = process.env.PORT || 4444;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running on ${port}/`);
+});
